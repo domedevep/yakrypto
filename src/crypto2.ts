3401ed6a22fd0154bcb33b651a745209d7c79c7f
Domenico Scoppettone <domedevep@gmail.com>
2024-11-04 20:34:29 -0400

    f

--- src/crypto2.ts
+++ src/crypto2.ts
@@ -1,7 +1,5 @@
 import EncryptDecrypt from "post_body_encrypt_decrypt";
 
-export const encryptDecrypt = new EncryptDecrypt();
-
-export const key = encryptDecrypt.generateSymmetricKey();
+export default EncryptDecrypt;
 
 
